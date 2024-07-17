263455cf6ae28ca396e5a0d2f825e653d865a799
Victor Santiago <vsantiag@redhat.com>
2024-07-17 18:45:10 -0500

    Added an option for group_vars.

--- Boilerplates/Dynamic_Inventory_Script/dynamic_inventory_script.py
+++ Boilerplates/Dynamic_Inventory_Script/dynamic_inventory_script.py
@@ -17,87 +17,111 @@ class AnsibleDynamicInventory:
                 ]
             },
             'ungrouped': {
-                'hosts': []
+                'hosts': [],
+                'vars': {}
             }
         }
 
     def __getitem__(self, item):
         return self.inventory[item]
 
-    def add_hosts(self, *, hosts: Optional[List[str]] = None, group: str, vars: Optional[Dict] = None) -> None:
+    def add_hosts(self, *, hosts: Optional[List[str]] = None, group: str, vars: Optional[Dict] = None, group_vars: Optional[Dict] = None) -> None:
         """
-        Add multiple hosts to a group with optional variables.
+        Add multiple hosts to a group with optional host and group variables.
 
         Args:
             group (str): The group to add the hosts to.
             hosts (List[str], optional): A list of host IPs or names.
             vars (Dict, optional): A dictionary of variables to associate with the hosts.
+            group_vars (Dict, optional): A dictionary of variables to associate with the group.
         """
         hosts = hosts or []
         vars = vars or {}
+        group_vars = group_vars or {}
+
+        self._ensure_group_exists(group=group, group_vars=group_vars)
 
         for host in hosts:
             self.add_host(host=host, group=group, vars=vars)
 
-    def add_host(self, *, host: str, group: Optional[str], vars: Optional[Dict] = None) -> None:
+    def add_host(self, *, host: str, group: Optional[str], vars: Optional[Dict] = None, group_vars: Optional[Dict] = None) -> None:
         """
-        Add a single host to a group with optional variables.
+        Add a single host to a group with optional host and group variables.
 
         Args:
             group (str): The group to add the host to.
             host (str): The host IP or name.
             vars (Dict, optional): A dictionary of variables to associate with the host.
+            group_vars (Dict, optional): A dictionary of variables to associate with the group.
         """
         vars = vars or {}
+        group_vars = group_vars or {}
 
         self.inventory['_meta']['hostvars'].setdefault(host, {}).update(vars)
 
         if group:
-            self._ensure_group_exists(group=group)
+            self._ensure_group_exists(group=group, group_vars=group_vars)
             if host not in self.inventory[group]['hosts']:
                 self.inventory[group]['hosts'].append(host)
         else:
             if host not in self.inventory['ungrouped']['hosts']:
                 self.inventory['ungrouped']['hosts'].append(host)
 
-    def add_host_to_group(self, *, host: str, group: str, vars: Optional[Dict] = None) -> None:
+    def add_host_to_group(self, *, host: str, group: str, vars: Optional[Dict] = None, group_vars: Optional[Dict] = None) -> None:
         """
-        Add an existing host to a group, or create and add the host if it doesn't exist.
+        Add an existing host to a group, or create and add the host if it doesn't exist, with optional host and group variables.
 
         Args:
             group (str): The group to add the host to.
             host (str): The host IP or name.
             vars (Dict, optional): A dictionary of variables to associate with the host.
+            group_vars (Dict, optional): A dictionary of variables to associate with the group.
         """
-        self.add_host(host=host, group=group, vars=vars)
+        self.add_host(host=host, group=group, vars=vars, group_vars=group_vars)
 
-    def add_child_group(self, *, parent_group: str, child_group: str) -> None:
+    def add_child_group(self, *, parent_group: str, child_group: str, group_vars: Optional[Dict] = None) -> None:
         """
-        Add a child group to an existing group.
+        Add a child group to an existing group with optional variables.
 
         Args:
             parent_group (str): The parent group to add the child group to.
             child_group (str): The child group to add.
+            group_vars (Dict, optional): A dictionary of variables to associate with the child group.
         """
         self._ensure_group_exists(group=parent_group)
-        self._ensure_group_exists(group=child_group)
+        self._ensure_group_exists(group=child_group, group_vars=group_vars)
 
         if 'children' not in self.inventory[parent_group]:
             self.inventory[parent_group]['children'] = []
         if child_group not in self.inventory[parent_group]['children']:
             self.inventory[parent_group]['children'].append(child_group)
 
-    def _ensure_group_exists(self, *, group: str) -> None:
+    def _ensure_group_exists(self, *, group: str, group_vars: Optional[Dict] = None) -> None:
         """
-        Ensure that a group exists in the 'all' children list.
+        Ensure that a group exists in the 'all' children list, with optional variables.
 
         Args:
             group (str): The group to ensure existence of.
+            group_vars (Dict, optional): A dictionary of variables to associate with the group.
         """
         if group not in self.inventory['all']['children']:
             self.inventory['all']['children'].append(group)
         if group not in self.inventory:
-            self.inventory[group] = {'hosts': []}
+            self.inventory[group] = {'hosts': [], 'vars': {}, 'children': []}
+        
+        if group_vars:
+            self.add_group_vars(group=group, vars=group_vars)
+
+    def add_group_vars(self, *, group: str, vars: Dict) -> None:
+        """
+        Add variables to a group.
+
+        Args:
+            group (str): The group to add the variables to.
+            vars (Dict): A dictionary of variables to associate with the group.
+        """
+        self._ensure_group_exists(group=group)
+        self.inventory[group]['vars'].update(vars)
 
     def get_devices(self) -> Dict[str, Dict]:
         """
@@ -149,6 +173,10 @@ def main():
             'ansible_host': '192.168.1.10',
             'ansible_user': 'user1',
             'ansible_ssh_private_key_file': '/path/to/private/key'
+        },
+        group_vars={
+            'group_var1': 'value1',
+            'group_var2': 'value2'
         }
     )
     example_inventory.add_hosts(
@@ -172,8 +200,14 @@ def main():
     # Example of adding an existing host to another group
     example_inventory.add_host_to_group(host='host1', group='group2')
 
-    # Example of adding a child group to a group
-    example_inventory.add_child_group(parent_group='group1', child_group='child_group1')
+    # Example of adding a child group to a group with variables
+    example_inventory.add_child_group(
+        parent_group='group1', child_group='child_group1',
+        group_vars={
+            'var1': 'value1',
+            'var2': 'value2'
+        }
+    )
 
     if args.list:
         print(json.dumps(example_inventory.inventory, indent=4))
@@ -183,8 +217,6 @@ def main():
                 args.host, {}
             ), indent=4
         ))
-
-    print('\n')
 
     # Loop through devices
     print("Devices:")
--- Boilerplates/Dynamic_Inventory_Script/test_ansible_dynamic_inventory.py
+++ Boilerplates/Dynamic_Inventory_Script/test_ansible_dynamic_inventory.py
@@ -7,26 +7,51 @@ class TestAnsibleDynamicInventory(unittest.TestCase):
         self.inventory = AnsibleDynamicInventory()
 
     def test_add_host(self):
-        self.inventory.add_host(host='host1', group='group1', vars={'var1': 'value1'})
+        self.inventory.add_host(host='host1', group='group1', vars={'var1': 'value1'}, group_vars={'group_var1': 'value1'})
         self.assertIn('host1', self.inventory.inventory['_meta']['hostvars'])
         self.assertIn('host1', self.inventory.inventory['group1']['hosts'])
         self.assertEqual(self.inventory.inventory['_meta']['hostvars']['host1']['var1'], 'value1')
+        self.assertEqual(self.inventory.inventory['group1']['vars']['group_var1'], 'value1')
 
     def test_add_hosts(self):
-        self.inventory.add_hosts(hosts=['host1', 'host2'], group='group1', vars={'var1': 'value1'})
+        self.inventory.add_hosts(hosts=['host1', 'host2'], group='group1', vars={'var1': 'value1'}, group_vars={'group_var1': 'value1'})
         self.assertIn('host1', self.inventory.inventory['_meta']['hostvars'])
         self.assertIn('host2', self.inventory.inventory['_meta']['hostvars'])
         self.assertIn('host1', self.inventory.inventory['group1']['hosts'])
         self.assertIn('host2', self.inventory.inventory['group1']['hosts'])
+        self.assertEqual(self.inventory.inventory['group1']['vars']['group_var1'], 'value1')
 
     def test_add_host_to_group(self):
-        self.inventory.add_host_to_group(host='host1', group='group1', vars={'var1': 'value1'})
+        self.inventory.add_host_to_group(host='host1', group='group1', vars={'var1': 'value1'}, group_vars={'group_var1': 'value1'})
         self.assertIn('host1', self.inventory.inventory['_meta']['hostvars'])
         self.assertIn('host1', self.inventory.inventory['group1']['hosts'])
+        self.assertEqual(self.inventory.inventory['group1']['vars']['group_var1'], 'value1')
 
     def test_add_child_group(self):
-        self.inventory.add_child_group(parent_group='group1', child_group='child_group1')
+        self.inventory.add_child_group(parent_group='group1', child_group='child_group1', group_vars={'var1': 'value1'})
         self.assertIn('child_group1', self.inventory.inventory['group1']['children'])
+        self.assertEqual(self.inventory.inventory['child_group1']['vars']['var1'], 'value1')
+
+    def test_ensure_group_exists(self):
+        self.inventory._ensure_group_exists(group='group1', group_vars={'var1': 'value1'})
+        self.assertIn('group1', self.inventory.inventory['all']['children'])
+        self.assertEqual(self.inventory.inventory['group1']['vars']['var1'], 'value1')
+
+    def test_get_devices(self):
+        self.inventory.add_host(host='host1', group='group1', vars={'var1': 'value1'})
+        devices = self.inventory.get_devices()
+        self.assertIn('host1', devices)
+        self.assertEqual(devices['host1']['var1'], 'value1')
+
+    def test_get_groups(self):
+        self.inventory.add_host(host='host1', group='group1', vars={'var1': 'value1'})
+        groups = self.inventory.get_groups()
+        self.assertIn('group1', groups)
+
+    def test_get_child_groups(self):
+        self.inventory.add_child_group(parent_group='group1', child_group='child_group1')
+        child_groups = self.inventory.get_child_groups('group1')
+        self.assertIn('child_group1', child_groups)
 
 
 if __name__ == '__main__':
